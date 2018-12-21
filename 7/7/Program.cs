7936acd5200f81068f3fd5619ff2fa13f959eea8
jarudat chaikuad <60030014@kmitl.ac.th>
2018-12-21 13:20:15 +0700

    ta

---
+++ 7/7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Student
+        {
+            // Fields declaration
+            private string name;
+            private string id;
+            private float gpa;
+
+            // read-only property
+            public string Name
+            {
+                get { return name; }
+            }
+
+            // read-only property
+            public string ID
+            {
+                get { return id; }
+            }
+
+            // read/write property
+            public float GPA
+            {
+
+            }
+        }
+}
